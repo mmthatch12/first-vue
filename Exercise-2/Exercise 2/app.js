697a0080e90717c98b293656efebe499e499f461
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2020-01-01 13:45:42 -0700

    working on exercise 2

---
+++ Exercise-2/Exercise 2/app.js
@@ -0,0 +1,11 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: ''
+        },
+        methods: {
+            clickbut: function(event){
+                alert("Button was clicked")
+            }
+        }
+    });
